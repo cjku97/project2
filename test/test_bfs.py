db90c5a7c4a9cfe059172cb5315f7a0c86b294c3
cjku97 <74573874+cjku97@users.noreply.github.com>
2022-01-10 16:06:22 -0800

    updated tests

--- test/test_bfs.py
+++ test/test_bfs.py
@@ -2,7 +2,7 @@
 import pytest
 from search import graph
 
-@pytest.fixture
+# @pytest.fixture
 def test_bfs_traversal():
     """
     TODO: Write your unit test for a breadth-first
@@ -11,7 +11,7 @@ def test_bfs_traversal():
     that all nodes are being traversed (ie. returns 
     the right number of nodes, in the right order, etc.)
     """
-    G = Graph("./data/tiny_network.adjlist")
+    G = graph.Graph("./data/tiny_network.adjlist")
     B = G.bfs("Lani Wu")
     assert len(B) == 30
     assert B == ['Lani Wu', '32042149', '32036252', '31806696', '30727954', 
@@ -20,7 +20,6 @@ def test_bfs_traversal():
     	'Charles Chiu', 'Martin Kampmann', 'Neil Risch', 'Nevan Krogan', 'Atul Butte', 
     	'Michael Keiser', '33242416', '32790644', '34272374', '32353859', '30944313', 
     	'33765435', '31395880', 'Marina Sirota', '31486345']
-    pass
 
 def test_bfs():
     """
@@ -33,8 +32,7 @@ def test_bfs():
     Include an additional test for nodes that are not connected 
     which should return None. 
     """
-    G = Graph("./data/tiny_network.adjlist")
+    G = graph.Graph("./data/tiny_network.adjlist")
     B = G.bfs("Lani Wu", "Steven Altschuler")
     assert len(B) == 3
     assert B == ['Lani Wu', '32036252', 'Steven Altschuler']
-    pass
